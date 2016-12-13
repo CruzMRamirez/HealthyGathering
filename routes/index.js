f807ba914363aee478602036b8c790fec0fd61bf
Cruz Ramirez <cruz_ramirez@ymail.com>
2016-12-13 15:37:20 -0800

    (changed nav and adding the db)

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,110 @@
 var express = require('express');
 var router = express.Router();
 
+// mongoose connection
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/test');
+
+
+var Schema = mongoose.Schema;
+
+// schema (blueprint)
+var userDataSchema = new Schema({
+  name: String,
+  content: String,
+  store: String
+});
+
+//model (access to the collection)
+var UserData = mongoose.model('UserData', userDataSchema);
+
+// insert (CRUD)
+router.post('/insert', function(req,res,next){
+  // var item = {
+  //   title : "To Kill A Mockingbird",
+  //   content : "This book was banned, Because it had bad words.",
+  //   author : "Harper Lee"
+  // };
+
+  var item = {
+    name : req.body.name,
+    content : req.body.content,
+    store : req.body.store
+  };
+
+  var data = new UserData(item);
+
+  data.save();
+  res.redirect('/test');
+
+});
+
+//
+router.get('/get-data', function(req,res,next){
+  UserData.find()
+    .then(function(doc){
+      res.render('index', {items: doc});
+    });
+});
+
+// get (CRUD)
+router.get('/data', function(req,res,next){
+  UserData.find()
+    .then(function(doc){
+      res.render('data', {items: doc});
+    });
+});
+
+//delete (CRUD)
+router.post('/delete', function(req,res,next){
+  var id = req.body.id;
+  UserData.findByIdAndRemove(id).exec();
+  res.redirect('/test');
+});
+
+//delete button
+router.get('/deletebtn/:id', function(req,res,next){
+  var id = req.params.id;
+  UserData.findByIdAndRemove(id).exec();
+  res.redirect('/test');
+});
+
+// Update (CRUD)
+router.post('/update', function(req,res,next){
+  var id = req.body.id;
+
+UserData.findById(id, function(err,doc){
+    if(err) {console.error('Error 404, Entry Not Found');}
+
+    doc.name = req.body.name;
+    doc.content = req.body.content;
+    doc.store = req.body.store;
+    doc.save();
+  });
+  res.redirect('/test');
+});
+/* GET new page */
+router.get('/new', function(req, res, next) {
+  res.render('new', { items: {} });
+});
+
+/* GET delete page */
+router.get('/deletedoc', function(req, res, next) {
+  res.render('deletedoc', { items: {} });
+});
+
+
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'index' });
+  res.render('index', { items: {} });
 });
 
 router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'test' });
+  res.render('test', { items: {} });
 });
 
 router.get('/about', function(req, res, next) {
@@ -34,4 +131,7 @@ router.get('/search', function(req, res, next) {
   res.render('search', { title: 'search' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
 module.exports = router;
